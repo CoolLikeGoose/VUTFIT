e5444156374da5268fd6a25809d2433f1923bec1
CoolLikeGoose <niken54@mail.ru>
2022-10-20 02:10:34 +0200

    Save contacts refactoring

--- t9search.c
+++ t9search.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAX_CONTACTS 42
 #define MAX_SYMB 100
 
-//debug
+char CharToNum(char);
+int SaveContacts(char[MAX_CONTACTS*2][MAX_SYMB]);
+
+// debug
 void print_arr(char arr[], int length) {
     printf(">");
-    for (int i = 0; i < length; i++) 
+    for (int i = 0; i < length; i++) {
         printf("%c", arr[i]);
+    }
     printf("\n");
 }
 
+// for (int i = 0; i < numOfContacts*2; i++) {
+//     print_contact_cell(contacts[i]);
+// }
+void print_contact_cell(char arr[]) {
+    printf(">");
+
+    int i = 0;
+    while (arr[i] != '\0') {
+        printf("%c", arr[i]);
+        i++;
+    }
+
+    printf("\n");
+}
+// debug
 
 int main(int argc, char *argv[]) {
+    printf("Program started\n");
     if (argc != 2) {
         fprintf(stderr, "Bad input! Number of arguments:%d", argc);
         exit(-1);
@@ -21,16 +42,21 @@ int main(int argc, char *argv[]) {
 
     //*2 because we contain NUMBER and NAME for each contact
     char contacts[MAX_CONTACTS*2][MAX_SYMB];
+    int numOfContacts = 0;
+    numOfContacts = SaveContacts(contacts);
+
     char *sequence = argv[1];
-    
-    //Get contacts from stdin in contacts array
+}
+
+//Get contacts from stdin in contacts array
+int SaveContacts(char contacts[MAX_CONTACTS*2][MAX_SYMB]) {
     char ch;
-    int numOfContacts = 0; 
     int subindex = 0;
+    int numOfContacts = 0;
     while ((ch = getchar()) != EOF) {
         if (ch == '\n') {
 
-            contacts[numOfContacts][subindex+1] = '\0'; //Define end of line
+            contacts[numOfContacts][subindex] = '\0'; //Define end of line
 
             numOfContacts++;
             subindex = 0;
@@ -38,5 +64,36 @@ int main(int argc, char *argv[]) {
         } 
         contacts[numOfContacts][subindex] = ch;
         subindex++;
+    }
+    numOfContacts++;
+    return numOfContacts/2;
+}
+
+//Get num representation of char
+//2 (abc), 3 (def), 4 (ghi), 5 (jkl), 6 (mno), 7 (pqrs), 8 (tuv), 9 (wxyz), 0 (+).
+char CharToNum(char ch) {
+    switch (tolower(ch))
+    {
+    case 'a': case 'b': case 'c':
+        return '2';
+    case 'd': case 'e': case 'f':
+        return '3';
+    case 'g': case 'h': case 'i':
+        return '4';
+    case 'j': case 'k': case 'l':
+        return '5';
+    case 'm': case 'n': case 'o':
+        return '6';
+    case 'p': case 'q': case 'r': case 's':
+        return '7';
+    case 't': case 'u': case 'v':
+        return '8';
+    case 'w': case 'x': case 'y': case 'z':
+        return '9';
+    case '+':
+        return '0';
+    default:
+        printf("Bad char!\n");
+        return '\0';
     }
 }
